37eb3ec61224fd36c3019a68979bc1daab38d705
bsdo <brenosill@hotmail.com>
2023-12-13 09:46:14 -0300

    Api sendo construida para consultar os dados do pokemon

--- web/assets/js/main.js
+++ web/assets/js/main.js
@@ -1,7 +1,22 @@
 
 const h1 = document.getElementById('btn');
 
-// <!-- addEventListener funcao pra diverso opces -->
-h1.addEventListener("click",function(){
-   alert("Ola");
-})
+// // <!-- addEventListener funcao pra diverso opces -->
+// h1.addEventListener("click",function(){
+//    alert("Ola");
+// })
+
+
+// fetch api pra consultar api
+
+const offset = 0;
+const limit = 10;
+const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+
+
+// Prommise => promessa de um resultado 
+fetch(url)
+   .then((response) => {return response.json()}) // transforma o retorno em json
+   .then((jsonBody) =>{console.log(jsonBody)}) // imprime o retorno de json
+   .catch(function (error){console.log(error) })
+   .finally(function (){console.log('Requisação concluida!')})
