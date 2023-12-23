e215e79a2c7a7e2e50bb79850334909f2e66a81c
bsdo <brenosill@hotmail.com>
2023-12-23 16:15:21 -0300

    Aula sobre aarray function e closures

---
+++ js avançado/funcao avancada/index.js
@@ -0,0 +1,21 @@
+// Arrow function 
+const funct = (a,b) => {return a + b}
+console.log(funct(10,33))
+
+// closures
+
+function soma (x)
+{
+    return function(y){
+        return x + y
+    }
+}
+
+const somaretorno = soma(50)
+
+// soma retotrno esta pegando a funcao do retorno para passa um valor
+console.log(somaretorno(220))
+console.log(somaretorno(21))
+console.log(somaretorno(30))
+
+console.log(soma(10)(50))
